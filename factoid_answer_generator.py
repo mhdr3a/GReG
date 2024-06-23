5494ec64ad6c2ccbcb4d92145f61bf32a6a33272
Mahdiyar Alavi <30527389+mhdr3a@users.noreply.github.com>
2024-06-23 19:01:01 -0400

    Update factoid_answer_generator.py

--- factoid_answer_generator.py
+++ factoid_answer_generator.py
@@ -52,10 +52,10 @@ def normalize(str1):
 def compare(str1, str2):
     return str1 == str2
 
-def estimate_cost(total_samples, fixed_instructions_msg, model, questions, max_output_tokens_per_sample, contexts=None, word_to_token_ratio=1.33, include_contexts=True):
+def estimate_cost(total_samples, fixed_instructions_msg, model, questions, max_output_tokens_per_sample, contexts=None, word_to_token_ratio=1.33, include_contexts=True, question_key='question'):
     fixed_instructions_tokens = word_to_token_ratio * len(fixed_instructions_msg.replace('\n', ' ').strip().split())
     total_context_tokens = word_to_token_ratio * sum(list(map(lambda x: sum(list(map(lambda y: len(y.replace('\n', ' ').strip().split()), x))), contexts))) if include_contexts else 0
-    total_input_tokens = word_to_token_ratio * sum(list(map(lambda q: len(q['question'].replace('\n', ' ').strip().split()), questions))) + total_samples * fixed_instructions_tokens + total_context_tokens
+    total_input_tokens = word_to_token_ratio * sum(list(map(lambda q: len(q[question_key].replace('\n', ' ').strip().split()), questions))) + total_samples * fixed_instructions_tokens + total_context_tokens
     total_output_tokens = total_samples * max_output_tokens_per_sample
     cost_per_1M_tokens_input = 5 if model == 'gpt-4o' else (.5 if model == 'gpt-3.5-turbo' else (10 if model == 'gpt-4-turbo' else None))
     cost_per_1M_tokens_output = 15 if model == 'gpt-4o' else (1.5 if model == 'gpt-3.5-turbo' else (30 if model == 'gpt-4-turbo' else None))
@@ -180,9 +180,13 @@ def main(opt):
     filepath = opt.data
     ext = filepath.split('.')[-1]
     dataset = filepath.split('/')[-3]
+    percentile = int(filepath.split('/')[-2].split('_')[-1])
     assert dataset in ['MuSiQue', 'HotpotQA', 'IIRC', '2WikiMultihopQA', 'NQ', 'TriviaQA']
     
     data, id_key, n = read_data(filepath, ext, dataset)
+
+    question_key = 'question' if percentile == 100 else 'question_org'
+    answer_key = 'answer' if percentile == 100 else 'answer_org'
     
     model = opt.model
     assert model in ['gpt-3.5-turbo', 'gpt-4o']
@@ -210,10 +214,10 @@ def main(opt):
     samples = []
     first_iter = True
     
-    for i in trange(3):
+    for i in trange(n):
         datum = data[i]
-        question = datum['question']
-        answer = datum['answer']
+        question = datum[question_key]
+        answer = datum[answer_key]
         id = datum[id_key]
         ##### <defining the prompt> #####
         full_prompt = ''; initial_prompt = ''
